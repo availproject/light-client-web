fc974639dac8078c7a30b28df49326fa9b49c2bb
Abheek Tripathy <abheek.tripathy@gmail.com>
2023-11-09 20:57:18 +0530

    ah fix

--- components/availchain.tsx
+++ components/availchain.tsx
@@ -1,11 +1,12 @@
+import Block from "./block";
+
 /* eslint-disable @next/next/no-img-element */
 export default function AvailChain() {
 
     return<>
     <div className="flex flex-row">
-    <img src="/images/block.png" className="w-100 h-100"></img>
-    <img src="/images/block.png" className="w-100 h-100"></img>
-    <img src="/images/block.png" className="w-100 h-100"></img>
+  <Block/>
+  
     </div>
     </>
 }
