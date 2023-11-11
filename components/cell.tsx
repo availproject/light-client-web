6136d5e8039df679f565b05c8ae718344709f974
vibhurajeev <vibhurajeev@gmail.com>
2023-11-11 11:36:09 +0400

    fix(matrix)

--- components/cell.tsx
+++ components/cell.tsx
@@ -1,6 +1,6 @@
 export default function Cell(props: any) {
     const color = props.color
     return (
-        <div style={{ backgroundColor: color }} className="w-3 h-3 rounded-lg "></div>
+        <div style={{ backgroundColor: color }} className="w-3 h-3 rounded-lg rounded-[2px]"></div>
     )
 }
--- components/dsmatrix.tsx
+++ components/dsmatrix.tsx
@@ -9,7 +9,6 @@ import { useWindowSize } from '@/hooks/use-window-size';
 
 
 export default function DsMatrix(props: any) {
-
   const windowSize = useWindowSize();
   let matrix = props.matrix
   let r = matrix.row
@@ -17,8 +16,8 @@ export default function DsMatrix(props: any) {
   let cells = matrix.verifiedCells
 
 
-  let row = new Array(parseInt(windowSize.width! < 760 ? '15' : windowSize.width! > 1500 ? '20' :'15')).fill(1)
-  let col = new Array(parseInt(windowSize.width! < 760 ? '20': windowSize.width! > 1500 ? '60':' 40')).fill(1)
+  let row = new Array(parseInt(windowSize.width! < 760 ? '15' : windowSize.width! > 1500 ? '20' : '15')).fill(1)
+  let col = new Array(parseInt(windowSize.width! < 760 ? '20' : windowSize.width! > 1500 ? '60' : ' 40')).fill(1)
 
   const checkForSampleCell = (row: any, col: any) => {
     return cells?.some((cell: { row: any; col: any; }) => {
@@ -35,65 +34,39 @@ export default function DsMatrix(props: any) {
     return "#222630"
   }
 
-  // useEffect(() => {
-  //   const cal: CalHeatmap = new CalHeatmap();
-  //   const dsMatrixDiv = document.getElementById('ds-matrix');
-  //   if (dsMatrixDiv) {
-  //     dsMatrixDiv.innerHTML = ''; // Clear the div
-  //   }
-  //   cal.paint(
-  //     {
-  //       theme: 'dark',
-  //       data: {
-  //         source: '/static/matrix.csv',
-  //         type: 'csv',
-  //         x: 'date',
-  //         y: (d: { [x: string]: string | number; }) => +d['temp_max'],
-  //         groupY: 'max',
-  //       },
-  //       date: { start: new Date('2012-01-01') },
-  //       range: 10,
-  //       scale: {
-  //         color: {
-  //           type: 'threshold',
-  //           range: ['#14432a', '#166b34', '#37a446', '#4dd05a'],
-  //           domain: [10, 20, 30],
-  //         },
-  //       },
-  //       domain: {
-  //         type: 'month',
-  //         gutter: 4,
-  //         label: { text: 'MMM', textAlign: 'start', position: 'top' },
-  //       },
-  //       subDomain: { type: 'ghDay', radius: 2, width: 11, height: 11, gutter: 4 },
-  //       itemSelector: '#ds-matrix',
-  //     })
-  // }, []);
+  const rows = () => {
+    let rows = [];
+    for (let i = 0; i < 10; i++) {
+      rows.push(
+        <div className="flex flex-row" key={i}>
+          {columns()}
+        </div>
+      );
+    }
+
+    return rows;
+  }
+
+  const columns = () => {
+    let rows = [];
+    for (let i = 0; i < 10; i++) {
+      rows.push(<Cell color={"#2DCF2A"} key={i} />)
+    }
+
+    return rows;
+  }
 
 
 
   return (
-    <div className="flex flex-col overflow-scroll lg:p-10 p-5 space-y-4 ">
-      <h1 className="subheading lg:!text-left !w-full 2xl:pb-2 pb-1 ">Data Sampling Matrix</h1>
-      <div className="rounded-xl p-2 bg-[#292E3A] ">
-      <div className="matrix p-1 ">
-
-        {
-          row.map((ele, i) => (
-            <div className="flex flex-row" key={i}>
-              {col.map((ele, j) => (
-                <div key={j} className="p-[1.6px]">
-                <Cell color={colorCheck(i, j)} key={j} />
-                </div>
-              ))}
-            </div>
-          ))
-
-        }
-      </div>
-      </div>
-      <div className="rounded-xl p-3 overflow-hidden text-[#292E3A]">
-        <div id="ds-matrix" className=""></div>
+    <div className="flex flex-col p-10 space-y-4">
+      <h1 className="subheading lg:!text-left !w-full 2xl:pb-2 pb-1">Data Sampling Matrix</h1>
+      <div className=" rounded-xl self-start p-4 bg-[#292E3A] max-h-[300px] lg:max-h-[600px] max-w-full">
+        <div className="matrix self-start   max-h-[268px] lg:max-h-[568px] overflow-auto">
+          {
+            rows()
+          }
+        </div>
       </div>
     </div>
   );
