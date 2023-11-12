643438ac085d377242eb3b965e3d3b0b72883e8c
Abheek Tripathy <abheek.tripathy@gmail.com>
2023-11-12 14:02:42 +0530

    progess bar sorted out

--- components/availchain.tsx
+++ components/availchain.tsx
@@ -1,3 +1,5 @@
+
+
 import { Block as block_type } from "@/types/light-client";
 import Block from "./block";
 import { Progress } from "./ui/progress";
@@ -11,17 +13,15 @@ type Props = {
 
 export default function AvailChain(props: Props) {
 
-
-
   const blockList = props.blockList
   return <>
     <div className="flex flex-row  overflow-auto whitespace-nowrap ">{
       blockList.map((block: block_type, index: number) => (
         <div key={index} className="flex flex-col items-center justify-center">
-          <h3 className="z-50 text-md text-white text-opacity-70 animate-pulse">#{block.number}</h3>
+          <h3 className={`z-50 text-md ${blockList.length > index + 1  ? 'text-white' : 'text-[#3CBBF9] animate-pulse'} text-opacity-70`}>#{block.number}</h3>
           <div className="relative">
             <Block key={index} progress={blockList.length > index + 1 ? 100 : 0} />
-            <Progress value={blockList.length > index + 1 ? 100 : 0} className="lg:h-2 lg:w-12 h-1 w-6 absolute left-0 top-1/2 transform -translate-y-1/2 !rounded-md"></Progress>
+            <Progress value={blockList.length > index + 1 ? 100 : (Date.now() % 20000) / 250} className="lg:h-2 lg:w-14 h-1 w-6 absolute right-0 top-1/2 transform -translate-y-1/2 !rounded-md"></Progress>
           </div>
         </div>
       ))}
