dc54ff843d9072ac816bbee5db3965fbff641c9c
Megumiiiiii <megumii6976@gmail.com>
2023-12-20 00:17:55 +0700

    Edit Block and Cell color

--- components/block.tsx
+++ components/block.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
+"use client"
 
 import { useState, useEffect } from "react";
 import { Progress } from "./ui/progress";
 import { Block } from "@/types/light-client";
+import { useTheme } from "next-themes";
 
 type Props = {
     progress: number
@@ -10,7 +12,17 @@ type Props = {
 
 
 export default function Block(props: Props) {
+    const { theme } = useTheme();
+    const [blockImage, setBlockImage] = useState("/images/block.png");
     const [value, setValue] = useState(0);
+
+    useEffect(() => {
+        if (theme === "dark") {
+          setBlockImage("/images/block-dark.png");
+        } else {
+          setBlockImage("/images/block-light.png");
+        }
+      }, [theme]);
 
     useEffect(() => {
         setValue(props.progress)
@@ -26,7 +38,7 @@ export default function Block(props: Props) {
         <div className="">
             <div className="flex flex-row items-center justify-center">
                 <img
-                    src="/images/block.png"
+                    src={blockImage}
                     alt="block"
                     className="2xl:max-h-[350px] lg:max-h-[250px] max-h-[120px] aspect-auto overflow-scroll overflow-y-auto"
                 />
