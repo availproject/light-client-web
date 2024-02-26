d38c08ba87a1fdb387f955449a61b107ee5dda4a
rishabhagrawalzra <rishabhagrawalzra@gmail.com>
2024-02-26 21:07:36 +0530

    fix: fixed the UI of DS matrix

--- components/dsmatrix.tsx
+++ components/dsmatrix.tsx
@@ -3,6 +3,7 @@
 import Cell from "./cell";
 import { Cell as CellType } from "@/types/light-client";
 import { Matrix } from "@/types/light-client";
+import config from "../utils/config"
 
 import { useWindowSize } from '@/hooks/use-window-size';
 
@@ -12,10 +13,17 @@ type Props = {
 }
 
 export default function DsMatrix(props: Props) {
+  // const matrix: Matrix = {
+  //   maxRow: 64,
+  //   maxCol: 64,
+  //   totalCellCount: 4096,
+  //   verifiedCells: []
+  // }
   let matrix: Matrix = props.matrix
   let columnCount = matrix.maxCol
   let cells = matrix.verifiedCells
   let totalCellCount = matrix.totalCellCount === 0 ? 128 : matrix.totalCellCount;
+
   let processing = props.processing;
 
   const windowSize = useWindowSize();
@@ -33,13 +41,18 @@ export default function DsMatrix(props: Props) {
     return (c as number > defaultColumnCount ? c : defaultColumnCount);
   }
 
-  let row = new Array(r).fill(1)
+  let row = new Array(r * config.EXTENSION_FACTOR).fill(1)
   let col = new Array(c).fill(1)
 
+  let totalCells = new Array(totalCellCount).fill(1)
+
   // function displayCells(): CellType[] {
+  //   const timeNow = Date.now();
   //   let displayCells: CellType[] = [];
   //   for (let i = 0; i < totalCellCount; i++) {
+  //     if (i == 1) console.log("Display cells is running")
   //     let index = i == 0 ? 1 : i;
+
   //     let row = Math.floor(index / columnCount);
   //     let col = index % columnCount;
 
@@ -49,6 +62,7 @@ export default function DsMatrix(props: Props) {
   //     });
   //   }
 
+  //   console.log(Date.now() - timeNow)
   //   return displayCells;
   // }
 
@@ -73,26 +87,33 @@ export default function DsMatrix(props: Props) {
       <h1 className="subheading lg:!text-left !w-full 2xl:pb-2 pb-1">Data Sampling Matrix</h1>
       <div className=" rounded-xl self-start p-4 bg-[#292E3A] max-h-[300px] lg:max-h-[600px] max-w-full">
         <div className="matrix flex flex-wrap self-start max-h-[268px] overflow-auto">
-          <div className="grid grid-cols-16 lg:grid-cols-32 gap-2">
-            {
+          {/* <div className="grid grid-cols-16 lg:grid-cols-32 gap-2"> */}
+          {
 
-              row.map((ele, i) => (
-                <div className="flex flex-row space-x-10" key={i}>
-                  {col.map((ele, j) => (
-                    <div key={j} className="p-[1.5px]">
-                      <Cell key={i * c + j} color={colorCheck(i, j)} />
-                      {/* <Cell key={i * c + j} color="#FFFF00" /> */}
-                    </div>
-                  ))}
-                </div>
-              ))
-              // displayCells().map((cell, index) => {
-              //   return (<Cell key={index} color={colorCheck(cell.row, cell.col)} />)
-              // })
-            }
-          </div>
+            row.map((ele, i) => (
+              <div className="flex flex-row" key={i}>
+                {col.map((ele, j) => (
+                  <div key={j} className="p-[1.5px]">
+                    <Cell key={i * c + j} color={colorCheck(i, j)} />
+                    {/* <Cell key={i * c + j} color="#FFFF00" /> */}
+                  </div>
+                ))}
+              </div>
+            ))
+            // displayCells().map((cell, index) => {
+            //   return (<Cell key={index} color={colorCheck(cell.row, cell.col)} />)
+            // })
+            // totalCells.map((ele, i) => {
+            //   let index = i == 0 ? 1 : i;
+
+            //   let row = Math.floor(index / columnCount);
+            //   let col = index % columnCount;
+            //   return (<Cell key={i} color={colorCheck(row, col)} />)
+            // })
+          }
+          {/* </div> */}
         </div>
       </div>
-    </div>
+    </div >
   );
 }
