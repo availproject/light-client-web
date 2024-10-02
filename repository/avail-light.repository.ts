3b1c80353d833f0b1e3cc1db163f5bc1687d8db0
rishabhagrawalzra <rishabhagrawalzra@gmail.com>
2024-10-02 12:55:41 +0530

    fix: kate proof u256 conversion to u8 array

--- repository/avail-light.repository.ts
+++ repository/avail-light.repository.ts
@@ -3,6 +3,8 @@ import config from "../utils/config"
 import { generateRandomCells } from '@/utils/helper';
 import { BlockToProcess } from '@/types/light-client';
 import { ApiPromise, initialize } from 'avail-js-sdk'
+import { Cell } from "@/types/light-client";
+import { bnToU8a } from '@polkadot/util';
 
 export async function runLC(onNewBlock: Function, registerUnsubscribe: Function): Promise<() => void> {
     const api: ApiPromise = await initialize('wss://turing-rpc.avail.so/ws');
@@ -20,29 +22,43 @@ export async function runLC(onNewBlock: Function, registerUnsubscribe: Function)
                 const r = commitment.rows
                 const c = commitment.cols
 
-                const totalCellCount = (r * config.EXTENSION_FACTOR) * c
-                let sampleCount = Math.min(config.SAMPLE_SIZE, totalCellCount)
-                const randomCells = generateRandomCells(r, c, sampleCount)
-
-                const kateProof = await api.rpc.kate.queryProof(randomCells, blockHash);
-
-                console.log(kateProof)
-
-                const kate_Proof = Uint8Array.from(kateProof)
+                // Generate commitment array
                 const kate_commitment = Uint8Array.from(kateCommitment.match(/.{1,2}/g).map((byte: string) => parseInt(byte, 16)))
-
                 let commitments: Uint8Array[] = []
                 for (let i = 0; i < (r * config.EXTENSION_FACTOR); i++) {
                     commitments.push(kate_commitment.slice(i * config.COMMITMENT_SIZE, (i + 1) * config.COMMITMENT_SIZE))
                 }
+
+                //Generating SAMPLE_SIZE random cell for sampling
+                const totalCellCount = (r * config.EXTENSION_FACTOR) * c
+                let sampleCount = config.SAMPLE_SIZE
+                if (sampleCount >= totalCellCount) {
+                    sampleCount = 5
+                }
+                const randomCells = generateRandomCells(r, c, sampleCount)
+
+                //For debugging
+                let cellList: Array<Cell> = []
+                cellList.push({ row: 0, col: 0 })
+
+                const kateProof = await api.rpc.kate.queryProof(randomCells, blockHash);
+
+                //console.log(kateProof)
+
+
                 let proofs: Uint8Array[] = []
                 for (let i = 0; i < sampleCount; i++) {
-                    proofs.push(kate_Proof.slice(i * config.KATE_PROOF_SIZE, (i + 1) * config.KATE_PROOF_SIZE))
+                    const byte32Array = bnToU8a(kateProof[i][0], { isLe: false })
+                    const byte80Array = new Uint8Array(80);
+                    byte80Array.set(kateProof[i][1], 0)
+                    byte80Array.set(byte32Array, 48)
+                    proofs.push(byte80Array)
                 }
 
                 const block: Block = { number: blockNumber, hash: blockHash, totalCellCount, confidence: 0, sampleCount, hasDaSubmissions: true }
                 const matrix: Matrix = { maxRow: r, maxCol: c, verifiedCells: [], totalCellCount }
-                onNewBlock((list: BlockToProcess[]) => [...list, { block, matrix, randomCells, proofs, commitments }])
+                //onNewBlock((list: BlockToProcess[]) => [...list, { block, matrix: null, randomCells: [], proofs: [], commitments: [] }])
+                onNewBlock((list: BlockToProcess[]) => [...list, { block, matrix, randomCells: randomCells, proofs, commitments }])
             } else {
                 const block: Block = { number: blockNumber, hash: blockHash, totalCellCount: 0, confidence: 0, sampleCount: 0, hasDaSubmissions: false }
                 onNewBlock((list: BlockToProcess[]) => [...list, { block, matrix: null, randomCells: [], proofs: [], commitments: [] }])
@@ -54,3 +70,4 @@ export async function runLC(onNewBlock: Function, registerUnsubscribe: Function)
 
     return registerUnsubscribe(() => unsubscribe);
 }
+
