f90e9e84e1e64ebc1624d96dec7434bd6c558e5a
Abheek Tripathy <abheek.tripathy@gmail.com>
2024-12-10 20:29:26 +0530

    chore: fix component structure

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ import { Block, Matrix, Cell, BlockToProcess } from "@/types/light-client";
 import { runLC } from "@/repository/avail-light.repository";
 import Link from "next/link";
 import React from "react";
+import Hero from "@/components/sections/hero";
 
 export default function Home() {
   const [network, setNetwork] = useState("Turing");
@@ -195,6 +196,7 @@ export default function Home() {
 
   return (
     <>
+      {/** Navbar with buttons and switcher */}
       <Navbar
         showButton
         button={
@@ -236,6 +238,7 @@ export default function Home() {
         }
       />
       <main className="">
+        {/** start lc button for mobile only */}
         <div className="md:hidden flex flex-col items-center justify-center py-8">
           <Button
             onClick={() => {
@@ -249,6 +252,7 @@ export default function Home() {
             {running ? "Stop Running the LC" : "Start Running the LC"}
           </Button>
         </div>
+        {/** core components */}
         {running && currentBlock != null ? (
           <div className="flex lg:flex-row flex-col-reverse lg:h-screen w-screen">
             <div className="lg:w-[60%] flex flex-col " id="blocks-section">
@@ -272,36 +276,7 @@ export default function Home() {
             </div>
           </div>
         ) : (
-          <>
-            {running && currentBlock === null ? (
-              <div className="flex items-center justify-center md:h-[80vh] h-[60vh]">
-                <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-white"></div>
-              </div>
-            ) : (
-              <div className="flex flex-col lg:p-16 p-6 2xl:p-20 space-y-10 2xl:space-y-14 ">
-                <h2 className="text-5xl 2xl:text-7xl font-thicccboibold leading-tight text-white !text-left lg:block ">
-                Trustlessly Verify Avail
-                </h2>
-                <p className="text-xl font-ppmori  text-white !text-left lg:block text-opacity-80 ">
-                Quickly and efficiently verify proofs from the <Link href={`https://www.availproject.org/`} className="text-[#3CBBF9] underline">Avail data availability layer</Link> give end users guarantees with cryptographic certainty that published data is both available and in its original form.
-                </p>
-                <p className="text-lg  font-ppmori  text-white !text-left lg:block text-opacity-80 ">
-                While most users cannot, or do not wish to run full nodes on their own devices, light clients are small enough to download and run on smartphones without relying on a centralized RPC provider or someone else’s full node. This can put <i>decentralized verification</i> into the palms of every user’s hand.
-
-                </p>
-                <p className="text-xl  2xl:text-4xl  font-ppmori text-white !text-left lg:block text-opacity-80 ">
-                  Check us on Twitter{" "}
-                  <Link
-                    href={"https://twitter.com/AvailProject"}
-                    className="text-[#3CBBF9]"
-                    target={"_blank"}
-                  >
-                    @AvailProject!
-                  </Link>
-                </p>
-              </div>
-            )}
-          </>
+          <Hero running={running} currentBlock={currentBlock} />
         )}
       </main>
     </>
